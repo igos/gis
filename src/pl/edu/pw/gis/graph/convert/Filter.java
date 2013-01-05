9bec696e2405ce884b81e96635f8209cdfb9984d
Igor Sawczuk <isawczuk@gmail.com>
2013-01-05 15:59:26 +0100

    / ‘bin/pl/edu/pw/gis/graph/convert/Filter.class’ / ‘src/pl/edu/pw/gis/graph/convert/Filter.java’

--- src/pl/edu/pw/gis/graph/convert/Filter.java
+++ src/pl/edu/pw/gis/graph/convert/Filter.java
@@ -4,7 +4,8 @@ import pl.edu.pw.gis.Graph;
 import pl.edu.pw.gis.GraphConvertStrategy;
 
 public class Filter implements GraphConvertStrategy {
-
+	
+	
 	@Override
 	public Graph convert(Graph graph) {
 		// TODO Auto-generated method stub
