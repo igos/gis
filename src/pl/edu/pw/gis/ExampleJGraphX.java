2471833e0d371e52d28a5daaa8aa2a74027da4e1
Igor Sawczuk <isawczuk@gmail.com>
2013-01-05 16:11:26 +0100

    / ‘bin/pl/edu/pw/gis/ExampleJGraphX.class’ / ‘src/pl/edu/pw/gis/ExampleJGraphX.java’

--- src/pl/edu/pw/gis/ExampleJGraphX.java
+++ src/pl/edu/pw/gis/ExampleJGraphX.java
@@ -1,5 +1,48 @@
 package pl.edu.pw.gis;
+import javax.swing.JFrame;
 
-public class ExampleJGraphX {
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.view.mxGraph;
+
+public class ExampleJGraphX extends JFrame
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2707712944901661771L;
+
+	public ExampleJGraphX()
+	{
+		super("Hello, World!");
+
+		mxGraph graph = new mxGraph();
+		Object parent = graph.getDefaultParent();
+
+		graph.getModel().beginUpdate();
+		try
+		{
+			Object v1 = graph.insertVertex(parent, null, "Hello", 20, 20, 80,
+					30);
+			Object v2 = graph.insertVertex(parent, null, "World!", 240, 150,
+					80, 30);
+			graph.insertEdge(parent, null, "Edge", v1, v2);
+		}
+		finally
+		{
+			graph.getModel().endUpdate();
+		}
+
+		mxGraphComponent graphComponent = new mxGraphComponent(graph);
+		getContentPane().add(graphComponent);
+	}
+
+	public static void main(String[] args)
+	{
+		ExampleJGraphX frame = new ExampleJGraphX();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 320);
+		frame.setVisible(true);
+	}
 
 }
