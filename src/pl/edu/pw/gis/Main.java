befb3b8decb93f03f17a96119e100b3481b5a38e
Igor Sawczuk <isawczuk@gmail.com>
2013-01-05 16:51:41 +0100

    / ‘bin/pl/edu/pw/gis/Main.class’ / ‘src/pl/edu/pw/gis/Main.java’

--- src/pl/edu/pw/gis/Main.java
+++ src/pl/edu/pw/gis/Main.java
@@ -6,6 +6,7 @@ import org.jgrapht.ListenableGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.ListenableDirectedGraph;
 
+import pl.edu.pw.gis.graph.Graph;
 import pl.edu.pw.gis.graph.JGraphXAdapter;
 
 import com.mxgraph.model.mxCell;
@@ -27,10 +28,10 @@ public class Main {
         g.addVertex( "v3" );
         g.addVertex( "v4" );
 
-        g.addEdge( "v1", "v2" );
-        g.addEdge( "v2", "v3" );
-        g.addEdge( "v3", "v1" );
-        g.addEdge( "v4", "v3" );
+        g.addWeightedEdge( "v1", "v2" );
+        g.addWeightedEdge( "v2", "v3" );
+        g.addWeightedEdge( "v3", "v1" );
+        g.addWeightedEdge( "v4", "v3" );
 
         JGraphXAdapter<String, DefaultEdge> graph = new JGraphXAdapter<String, DefaultEdge>(g);
 
@@ -41,10 +42,10 @@ public class Main {
         frame.setSize(400, 320);
         frame.setVisible(true);
 
-        g.addVertex( "v5" );
-        g.addVertex( "v6" );
-        g.addVertex( "v7" );
-        g.addVertex( "v8" );
+//        g.addVertex( "v5" );
+//        g.addVertex( "v6" );
+//        g.addVertex( "v7" );
+//        g.addVertex( "v8" );
 
         graph.getModel().beginUpdate();
         double x = 20, y = 20;
