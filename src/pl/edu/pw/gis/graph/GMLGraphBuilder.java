4aa79c35b52386ded525b0ffe0397d919c9603c9
profetes <ja@profetes.pl>
2013-01-09 20:43:02 +0100

    handling missing 'weight' property in GML, code formatting

--- src/pl/edu/pw/gis/graph/GMLGraphBuilder.java
+++ src/pl/edu/pw/gis/graph/GMLGraphBuilder.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 
 import org.jgrapht.graph.DefaultWeightedEdge;
 
@@ -18,31 +17,41 @@ public class GMLGraphBuilder {
 
 	private Graph<String, DefaultWeightedEdge> wrapped;
 	private TinkerGraph graph;
-	
-	public GMLGraphBuilder(Graph<String,DefaultWeightedEdge> wrapped) {
+
+	public GMLGraphBuilder(Graph<String, DefaultWeightedEdge> wrapped) {
 		this.wrapped = wrapped;
 		graph = new TinkerGraph();
 	}
-	
+
 	public void readGMLFile(String filepath) throws IOException {
 		GMLReader gmlReader = new GMLReader(graph);
-		if (! new File(filepath).canRead()) {
-			throw new FileNotFoundException("file '" + filepath + "' cannot be opened for read");
+		if (!new File(filepath).canRead()) {
+			throw new FileNotFoundException("file '" + filepath
+					+ "' cannot be opened for read");
 		}
 		gmlReader.inputGraph(new FileInputStream(filepath));
-		for (Vertex v: graph.getVertices()) {
+		for (Vertex v : graph.getVertices()) {
 			String id = (String) v.getId();
 			System.out.println("loaded vertex: " + id);
 			wrapped.addVertex(id);
 		}
-		
-		for(Edge e: graph.getEdges()) {
+
+		for (Edge e : graph.getEdges()) {
 			String id = (String) e.getId();
 			Number weight = (Number) e.getProperty("weight");
-			System.out.println("loaded edge: " + id + ", from " + e.getVertex(Direction.IN).getId() + " to " + e.getVertex(Direction.OUT).getId() + " with weight " + weight);
+			if (weight == null) {
+				throw new IOException(
+						"Invalid GML file format, each edge should has 'weight' property");
+			}
+			System.out.println("loaded edge: " + id + ", from "
+					+ e.getVertex(Direction.IN).getId() + " to "
+					+ e.getVertex(Direction.OUT).getId() + " with weight "
+					+ weight);
 
 			// add edge, read edge's value first
-			wrapped.addWeightedEdge(e.getVertex(Direction.IN).getId().toString(), e.getVertex(Direction.OUT).getId().toString(), weight.doubleValue());
+			wrapped.addWeightedEdge(e.getVertex(Direction.IN).getId()
+					.toString(), e.getVertex(Direction.OUT).getId().toString(),
+					weight.doubleValue());
 		}
 	}
 }
